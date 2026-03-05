1fc03baa80683948a1fdbf61c6bd5f5d1e4bf69e
codewithshabbir@gmail.com <codewithshabbir@gmail.com>
2026-03-05 17:56:23 +0530

    working mail, setup production guidelines

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,15 +8,3 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
-
-
-// const mysql = require("mysql2");
-
-// const pool = mysql.createPool({
-//   uri: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// module.exports = pool.promise();
