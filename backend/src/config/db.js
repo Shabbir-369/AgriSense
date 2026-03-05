2e729f008699949dc3eceebc474b699983431ae7
codewithshabbir@gmail.com <codewithshabbir@gmail.com>
2026-03-05 17:39:29 +0530

    fixed db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,22 +1,22 @@
-// const mysql = require("mysql2");
-
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// module.exports = pool.promise();
-
-
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool.promise();
+
+
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   uri: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// module.exports = pool.promise();
